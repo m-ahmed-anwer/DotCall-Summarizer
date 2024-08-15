830d395775694c7d899cd8bc613c18da28185e08
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-08-15 12:57:30 +0530

    Update Changes

--- api/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class CallSummarySerializer(serializers.Serializer):
-    transcription = serializers.CharField()
-    summary = serializers.CharField(read_only=True)
